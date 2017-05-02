8003a84de8b94f998d213b7a83160fa8372f6c84
ri-mo- <ri-mo-@pixiv.com>
2017-05-02 16:16:33 +0900

    サイト名表示

--- trend.php
+++ trend.php
@@ -4,21 +4,24 @@ $RSSpath = [
   'https://queryfeed.net/twitter?q=from%3AKAI_YOU_ed&title-type=tweet-text-full&geocode=',
   'https://queryfeed.net/twitter?q=from%3AAnimeAnime_jp&title-type=tweet-text-full&geocode='
 ];
-
+$sitenames = [
+  'https://queryfeed.net/twitter?q=from%3AKAI_YOU_ed&title-type=tweet-text-full&geocode='=>'KAIYOU',
+  'https://queryfeed.net/twitter?q=from%3AAnimeAnime_jp&title-type=tweet-text-full&geocode='=>'アニメ！アニメ！'
+];
 foreach ($RSSpath as $url) {
   $XML = simplexml_load_file ( $url, 'SimpleXMLElement', LIBXML_NOCDATA );
 
   $x=0; // カウントをリセット
   $num=3; // 指定した数で終了
-
-foreach ( $XML->channel->item as $item ) {
-    if ( $x>=$num ) { // 指定した数とマッチしたら終了
-    break;
-    }
+  $tweets = $tweets.$sitenames[$url]."\n";
+  foreach ( $XML->channel->item as $item ) {
+    if ( $x>=$num ) {
+      $tweets = $tweets."\n";
+      break;
+   }
     $x++; // カウントの追加
     $title = $item->title;
     $tweets = $tweets.$title."\n";
-
   }
 }
 $url = 'https://hooks.slack.com/services/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';
